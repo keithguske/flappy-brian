8187eb9f738953916d9a4f02abc63a43bcff08ee
Keith Guske <keith@guske.net>
2015-09-10 16:38:15 -0400

    First Commit

---
+++ distribute.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+love-release -W -l 0.9.2 -t "flappy-brian" -i graphics/brianbird.png -x distribute.sh -v 0.0.2
+love-release -M -l 0.9.2 -t "flappy-brian" -i graphics/brianbird.png -x distribute.sh -v 0.0.2
+love-release -D -l 0.9.2 -t "flappy-brian" -i graphics/brianbird.png -x distribute.sh -v 0.0.2 -u briangatto.party -d "\"It's like Flappy Bird, but with Brian\" -IGN 9/10" -e keith.guske@gmail.com
